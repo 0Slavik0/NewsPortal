71ed8b5987badca0341a6edf90fbeb261d7770c6
Slavik <solovyework@gmail.com>
2023-03-02 21:14:48 +0300

    Add files via upload

---
+++ NewsPortal/NEWS/resourser.py
@@ -0,0 +1,27 @@
+culture = 'CU'
+science = 'SC'
+tech = 'TE'
+politics = 'PO'
+sport = 'SP'
+entertainment = 'EN'
+economics = 'EC'
+education = 'ED'
+
+CATEGORIES = [
+    (culture, 'Культура'),
+    (science, 'Наука'),
+    (politics, 'Политика'),
+    (sport, 'Спорт'),
+    (economics, 'Экономика'),
+    (education, 'Развлечения'),
+    (tech, 'Технологии'),
+    (entertainment, 'Образование')
+]
+
+article = 'AR'
+news = 'NE'
+
+POST_TYPES = [
+    (article, 'Статья'),
+    (news, 'Новость')
+]
